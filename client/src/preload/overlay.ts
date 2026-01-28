1298026e104d8ba65fc49ef808620cadd45b628b
YanicB <yanic.ballez@gmail.com>
2026-01-27 19:18:20 -0500

    add styling

--- client/src/preload/overlay.ts
+++ client/src/preload/overlay.ts
@@ -1,7 +1,89 @@
 /**
- * Placeholder preload script for overlay window
- * Will be properly implemented in Phase 6
+ * Preload script for overlay window
+ * Exposes type-safe IPC APIs to renderer process
  */
 
-// For now, just re-export from the existing preload script
-console.log('Overlay preload loaded (placeholder)');
+import { contextBridge, ipcRenderer } from 'electron';
+import { IPC_CHANNELS } from '../shared/constants/channels';
+import type { ElectronAPI } from '../shared/types/ipc.types';
+
+const electronAPI: ElectronAPI = {
+  // Tutorial commands
+  startTutorial: (task: string) =>
+    ipcRenderer.invoke(IPC_CHANNELS.START_TUTORIAL, task),
+
+  cancelTutorial: () => {
+    ipcRenderer.send(IPC_CHANNELS.CANCEL_TUTORIAL);
+  },
+
+  notifyTargetClicked: () => {
+    ipcRenderer.send(IPC_CHANNELS.TARGET_CLICKED);
+  },
+
+  // Tutorial state listeners
+  onTutorialStateChange: (callback) => {
+    const listener = (_event: Electron.IpcRendererEvent, state: any) => callback(state);
+    ipcRenderer.on(IPC_CHANNELS.TUTORIAL_STATE_CHANGE, listener);
+    return () => {
+      ipcRenderer.removeListener(IPC_CHANNELS.TUTORIAL_STATE_CHANGE, listener);
+    };
+  },
+
+  onSetSpotlightPosition: (callback) => {
+    const listener = (_event: Electron.IpcRendererEvent, coords: any) => callback(coords);
+    ipcRenderer.on(IPC_CHANNELS.SET_SPOTLIGHT_POSITION, listener);
+    return () => {
+      ipcRenderer.removeListener(IPC_CHANNELS.SET_SPOTLIGHT_POSITION, listener);
+    };
+  },
+
+  // Mouse event listeners
+  onGlobalMouseMove: (callback) => {
+    const listener = (_event: Electron.IpcRendererEvent, event: any) => callback(event);
+    ipcRenderer.on(IPC_CHANNELS.GLOBAL_MOUSE_MOVE, listener);
+    return () => {
+      ipcRenderer.removeListener(IPC_CHANNELS.GLOBAL_MOUSE_MOVE, listener);
+    };
+  },
+
+  onGlobalMouseDown: (callback) => {
+    const listener = (_event: Electron.IpcRendererEvent, event: any) => callback(event);
+    ipcRenderer.on(IPC_CHANNELS.GLOBAL_MOUSE_DOWN, listener);
+    return () => {
+      ipcRenderer.removeListener(IPC_CHANNELS.GLOBAL_MOUSE_DOWN, listener);
+    };
+  },
+
+  onGlobalMouseUp: (callback) => {
+    const listener = (_event: Electron.IpcRendererEvent, event: any) => callback(event);
+    ipcRenderer.on(IPC_CHANNELS.GLOBAL_MOUSE_UP, listener);
+    return () => {
+      ipcRenderer.removeListener(IPC_CHANNELS.GLOBAL_MOUSE_UP, listener);
+    };
+  },
+
+  onGlobalClick: (callback) => {
+    const listener = (_event: Electron.IpcRendererEvent, event: any) => callback(event);
+    ipcRenderer.on(IPC_CHANNELS.GLOBAL_CLICK, listener);
+    return () => {
+      ipcRenderer.removeListener(IPC_CHANNELS.GLOBAL_CLICK, listener);
+    };
+  },
+
+  onGlobalScroll: (callback) => {
+    const listener = (_event: Electron.IpcRendererEvent, event: any) => callback(event);
+    ipcRenderer.on(IPC_CHANNELS.GLOBAL_SCROLL, listener);
+    return () => {
+      ipcRenderer.removeListener(IPC_CHANNELS.GLOBAL_SCROLL, listener);
+    };
+  },
+
+  // Window control
+  send: (channel: string, data: unknown) => {
+    ipcRenderer.send(channel, data);
+  },
+};
+
+contextBridge.exposeInMainWorld('electronAPI', electronAPI);
+
+console.log('Overlay preload script loaded');
